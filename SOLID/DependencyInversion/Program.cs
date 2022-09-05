47185d53f78e7d1b67e18ecd33f69e4ee66dca87
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-09-05 15:59:33 +0300

    Last commit

---
+++ SOLID/DependencyInversion/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Report report = new Report(new MMSSender());
+Report rp2 = new Report(new MailSender());
+Report rp3 = new Report(new MMSSender());
+
+
+public class Report
+{
+   ISender sender;
+
+    public Report(ISender sender)
+    {
+        this.sender = sender;
+    }
+
+    public void Send(string to)
+    {
+       
+        sender.Send(to);
+    }
+}
+public interface ISender
+{
+    void Send(string to);
+}
+public class MailSender: ISender
+{
+    public void Send(string to)
+    {
+        Console.WriteLine($"{to} adresine rapor gönderildi");
+    }
+}
+
+public class WhatsappSender : ISender
+{
+    public void Send(string to)
+    {
+        Console.WriteLine($"{to} adresine rapor Whatsapp ile gönderildi");
+    }
+}
+
+public class MMSSender : ISender
+{
+    public void Send(string to)
+    {
+        Console.WriteLine($"{to} adresine rapor MMS ile gönderildi");
+    }
+}
---
+++ SOLID/InterfaceSegregation/Program.cs
@@ -0,0 +1,43 @@
+﻿//Bir fonksiyon; mutlaka bir interface'a bağlı olmamalı. Fakat eğer bir interface'i implemente ettiği için geliyorsa mutlaka kullanılmalı.
+
+public interface IMessage
+{
+    string From { get; set; }
+    string To { get; set; }
+    string Subject { get; set; }
+    string Body { get; set; }
+
+
+
+}
+
+public interface IVideoMessage : IMessage
+{
+    int VideoDuration { get; set; }
+    string VideoFormat { get; set; }
+}
+
+public interface IImageMessage: IMessage
+{
+    int ImageBytes { get; set; }
+    string ImageType { get; set; }
+}
+
+
+public class TextMessage : IMessage
+{
+    public string From { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string To { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string Subject { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string Body { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+}
+
+public class VideoMessage : IVideoMessage
+{
+    public int VideoDuration { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string VideoFormat { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string From { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string To { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string Subject { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string Body { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+}
